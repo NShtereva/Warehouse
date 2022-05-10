7a35e2d27339f8d6875e7236425d6fdc65bbc71b
NShtereva <niki1shtereva@gmail.com>
2022-05-10 21:53:45 +0300

    add class Constants

---
+++ constants.hpp
@@ -0,0 +1,27 @@
+#ifndef __CONSTANTS_HPP
+#define __CONSTANTS_HPP
+
+#include <iostream>
+
+class Constants
+{
+    public:
+        Constants()= delete;
+        
+        static const size_t MAX_NAME_LEN = 255;
+        static const size_t MAX_MAKER_LEN = 255;
+        static const size_t MAX_COMMENT_LEN = 10000;
+
+        static const size_t INITIAL_CAPACITY = 2;
+        static const size_t INCREASE_STEP = 2;
+
+        static const size_t MAX_SHELF_CAPACITY = 16;
+        static const size_t MAX_QUANTITY_IN_ONE_SHELF_DIVISION = 9;
+
+        static const size_t MAX_SECTION_CAPACITY = 16;
+
+        static const size_t MAX_BUFFER_LEN = 10000;
+        static const size_t MAX_OPERATION_LEN = 20;
+};
+
+#endif
